2b059a260dfe699185d87d85fbab7d11ccc4d501
Nico Caprioli <nico.caprioli@gmail.com>
2026-05-09 09:06:00 +0200

    fix: eslint
    Signed-off-by: Nico Caprioli <nico.caprioli@gmail.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,43 @@
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import astro from "eslint-plugin-astro";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-]);
+export default [
+  // Node config files
+  {
+    files: ["*.config.ts"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  // Astro files
+  {
+    files: ["**/*.astro"],
+    processor: astro.processors.astro,
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...astro.configs.recommended,
+    },
+  },
+
+  // TypeScript files
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
